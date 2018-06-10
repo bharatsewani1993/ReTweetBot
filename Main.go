f6ae4c2b5cef6011bbacfb245634189e50a8b7bd
BHARAT <bharatsewani1993@gmail.com>
2018-06-10 21:25:34 +0530

    Follow back the follower

--- Main.go
+++ Main.go
@@ -4,22 +4,31 @@ import (
     "github.com/ChimeraCoder/anaconda"
     "fmt"
     "net/url"
+    "reflect"
+    "database/sql"
+    _ "github.com/go-sql-driver/mysql"
+    "strconv"
+    "strings"
 )
 
 
-
-func main(){
-   //API Key and Access Token
+//login to twitter
+func twitterlogin() (*anaconda.TwitterApi) {
+  //API Key and Access Token
   consumerkey := ""
   consumersecret := ""
   accesstoken := ""
   accesstokensecret := ""
 
-  //Authentication With Twitter
-  anaconda.SetConsumerKey(consumerkey)
-  anaconda.SetConsumerSecret(consumersecret)
-  api := anaconda.NewTwitterApi(accesstoken,accesstokensecret)
+ //Authentication With Twitter
+ anaconda.SetConsumerKey(consumerkey)
+ anaconda.SetConsumerSecret(consumersecret)
+ return anaconda.NewTwitterApi(accesstoken,accesstokensecret)
+}
 
+
+//search and retweet
+func searchandretweet(api *anaconda.TwitterApi){
   //define search term
   options := url.Values{}
   options.Set("count","150")
@@ -34,8 +43,75 @@ func main(){
   for _, tweet := range SearchResult.Statuses{
      fmt.Printf("Tweet %v\n",tweet.Text)
      //ReTweet
-     tweetinfo, _ := api.Retweet(tweet.Id,true)
-     fmt.Printf(tweetinfo.Text)
+    // tweetinfo, _ := api.Retweet(tweet.Id,true)
+     //fmt.Printf(tweetinfo.Text)
   }
+}
 
+func followbackusers(api *anaconda.TwitterApi, db *sql.DB){
+    //get list of followers
+    options := url.Values{}
+    options.Set("skip_status","true")
+    options.Set("include_user_entities","false")
+    options.Set("count","200")
+
+    //get all followers from Twitter
+    followerlist := <-api.GetFollowersIdsAll(options)
+
+    //check for new followers by comparing with existing in database
+      followersindb := getfollowersfromdb(db)
+      fmt.Printf("%v",followersindb)
+
+    //loop through follower list
+    options.Del("skip_status")
+    options.Del("include_user_entities")
+    options.Del("count")
+    options.Set("follow","false")
+    for _, list := range followerlist.Ids{
+      //fmt.Printf("User Id %v\n",list)
+      //follow back users
+      api.FollowUserId(list,options)
+    }
+    //add users to database
+    storefollowersid(followerlist.Ids,db)
+}
+
+//initialize database connection
+func dbconnection() (*sql.DB) {
+  db, _ := sql.Open("mysql","root:P3NT3ST3R@/retweetbot")
+  return db
+}
+
+func storefollowersid(followerids []int64, db *sql.DB){
+   //genrate query string
+     querystring := "("
+     for _, followerid := range followerids {
+       s := strconv.FormatInt(followerid,10)
+       querystring += s + "),("
+     }
+     querystring = strings.TrimRight(querystring, ",(")
+     fmt.Println("Query String %v\n",querystring)
+
+    //prepare batch insert statement
+      insertqry, _ := db.Prepare("INSERT into followers values " + querystring)
+      fmt.Println("Query to execute %v\n",insertqry)
+}
+
+func getfollowersfromdb(db *sql.DB) ([]int64){
+   var followersindb []int64
+   rows, _ := db.Query("SELECT * from followers where active = '1'")
+   for rows.Next(){
+     var followerid int64
+       _ = rows.Scan(&followerid)
+      followersindb = append(followersindb,followerid)
+   }
+   return followersindb
+}
+
+func main(){
+  api := twitterlogin()
+  db := dbconnection()
+  searchandretweet(api)
+  followbackusers(api,db)
+  fmt.Println(reflect.TypeOf(api))
 }
--- README.md
+++ README.md
@@ -4,10 +4,10 @@ It is Golang retweet bot. It's search for the hashtag #Golangjob and retweets it
 
 ## Current Features
 1. Searches for the tweets with hashtag #Golangjob and retweet them.
+2. Follow back the Follower.
 
 ## ToDo
 1. Create a Cron job to auto-execute it.
-2. Follow back the Follower.
-3. Greet the follower.
-4. Setup it using Docker.
-5. Test the Code and put it on a production server.
+2. Greet the follower.
+3. Setup it using Docker.
+4. Test the Code and put it on a production server.
